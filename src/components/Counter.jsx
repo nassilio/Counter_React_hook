1a4155eda866a37a4083869b3de52c0f4c3724ca
nassilio <55001600+nassilio@users.noreply.github.com>
2019-11-12 14:43:33 +0100

    Counter React hook done

---
+++ src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function Counter() {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  // Déclare une fonction setCount qui permet de modifier la valeur de count
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Le compteur est à : {count + 3} </p>
+      <input value={count + 3} onChange={e => setCount(e.target.value)}></input>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+    </div>
+  );
+}
+
+export default Counter;
